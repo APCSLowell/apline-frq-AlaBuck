cf16e80116855a8c0e56c0b97989982aa5f89af3
AlaBuck <Albuckingham@s.sfusd.edu>
2025-05-22 10:59:13 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -2,19 +2,18 @@ public class APLine
 {
    private int a,b,c;
    
-   public APLine(int a1, int b1, int c1){   
-      int a = a1;
-      int b = b1;
-      int c = c1;
+   public APLine(int a1, int b1, int c1)
+   {   
+       a = a1;
+       b = b1;
+       c = c1;
    } 
-   public double getSlope(){
+   public double getSlope()
+   {
       return -(double)a/b;
    }
-   public boolean isOnLine(int x, int y){
-      if(a*x+b*y+c==0){
-         return true;
-      } else {
-         return false;
-      }
+   public boolean isOnLine(int x, int y)
+   {
+      return a*x+b*y+c==0;
    }
 }
